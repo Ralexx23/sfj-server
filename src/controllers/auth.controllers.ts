b85bed8527c6760cf0a09381e3deba7f6adcb5db
FritoLay23 <alexanderlop790@gmail.com>
2023-05-02 18:29:06 -0400

    other commit

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,58 +1,56 @@
-import { Response } from 'express';
-import { custReq } from '../utils/requestExtend';
-import jwt from 'jsonwebtoken';
-import UserModel from '../models/user.models';
-import Encrypt from '../utils/encrypt';
+import { Response } from "express";
+import { custReq } from "../utils/requestExtend";
+import jwt from "jsonwebtoken";
+import UserModel from "../models/user.models";
+import Encrypt from "../utils/encrypt";
 
 export const Login = async (req: custReq, res: Response) => {
-    //Received the req.body
-    //Validation the information with Zod
-    //Store in database
-    const authData = req.body.auth;
-    if(!authData){
-        console.log('no auth data');
-        console.log(authData);
-        res.status(400).send({msg: 'no auth data'});
-        return;
-    }else if(!authData.user || !authData.password) {
-        res.status(404).send({msg: 'user or password not provided'});
-        return;
+  //Received the req.body
+  //Validation the information with Zod
+  //Store in database
+  const authData = req.body.auth;
+  if (!authData) {
+    console.log("no auth data");
+    console.log(authData);
+    res.status(400).send({ msg: "no auth data" });
+    return;
+  } else if (!authData.user || !authData.password) {
+    res.status(404).send({ msg: "user or password not provided" });
+    return;
+  }
+
+  const user = await UserModel.findOne({ user: authData.user });
+  if (!user) {
+    res.status(400).send({ msg: "user not found" });
+    return;
+  }
+
+  const check = await Encrypt.compare(authData.password, user.password);
+  if (!check) {
+    console.log("password not match");
+    res.status(400).send({ msg: "password not match" });
+    return;
+  }
+
+  //Generate token
+  const token = jwt.sign(
+    {
+      user: authData,
+    },
+    "secret",
+    {
+      expiresIn: 60 * 60 * 24,
     }
+  );
 
-    const user = await UserModel.findOne({'user': authData.user});
-    if(!user){
-        res.status(400).send({msg: 'user not found'});
-        return;
-    }
-
-    /*const check = await Encrypt.compare(authData.password, user.password);
-    if(!check){
-        console.log('password not match');
-        res.status(400).send({msg: 'password not match'});
-        return;
-    }*/
-
-    if(authData.password === user.password){
-        console.log('contraseña correcta');
-    }else{
-        console.log('contraseña incorrecta');   
-    }
-
-    //Generate token
-    const token = jwt.sign({
-        user: authData,
-    }, "secret", {
-        expiresIn: 60 * 60 * 24
-    });
-
-    return res.json({
-        token
-    });
+  return res.json({
+    token,
+  });
 };
 
 export const GetAuth = (req: custReq, res: Response) => {
-    return res.json({
-        user: req.user,
-        msg: "Auth Data"
-    });
+  return res.json({
+    user: req.user,
+    msg: "Auth Data",
+  });
 };
